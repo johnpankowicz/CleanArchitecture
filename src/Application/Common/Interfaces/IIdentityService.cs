696ad91fe42cafc7de892c9bfc22337fa9176f40
johnpankowicz <johnpankowicz@hotmail.com>
2020-10-13 11:42:35 -0400

    Initialize authorization

---
+++ src/Application/Common/Interfaces/IIdentityService.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchitecture.Application.Common.Models;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Common.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<string> GetUserNameAsync(string userId);
+
+        Task<bool> UserIsInRole(string userId, string role);
+
+        Task<(Result Result, string UserId)> CreateUserAsync(string userName, string password);
+
+        Task<Result> DeleteUserAsync(string userId);
+    }
+}
